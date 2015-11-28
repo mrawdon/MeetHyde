5ba240b721a7c07989d167a3fa36d3d90543f0b7
Joshua Vaage <joshuavaage@gmail.com>
2015-11-27 22:37:59 -0800

    oops

--- src/assets/js/meet-hyde/config.js
+++ src/assets/js/meet-hyde/config.js
@@ -1,5 +1,5 @@
 //Place here your personal Access Token, you can go to our Readme if you need help
-	var AccessToken = '3e845474bc3059bae2086610d182f608c1340b34';
+	var AccessToken = 'your-token';
 
 //Html tab size
 	var htmlEditorTabSize = '2';
